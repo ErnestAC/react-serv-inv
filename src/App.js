65d6356f9375f3f32b620eca9b20b8b28d00f0f8
ErnestAC <rabergcobo@gmail.com>
2022-08-18 18:31:27 -0300

    Added a navbar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import logo from './app.png';
 import './App.css';
 
+// COMPONENTS
+import NavBar from "./components/NavBar";    
+
+//RANDOM CONSTANT
 const vVolume = 100;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>REACT SERVER INVENTORY</h1>
+        <h1>LMODELS ECOMMERCE</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {vVolume}
+          <NavBar />
+          Some text and a the value of a constant: {vVolume}
         </p>
         <p>
-          I want to add React to <br></br>
-          <a
-          className="App-link"
-          href="https://ernestac.github.io/serv.inventory/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          this.
-        </a>
         </p>
       </header>
     </div>
